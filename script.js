28257e611308b7ecb5eec7c8af3f6b3e48aa1ca3
LOAD-13 <104539394+LOAD-13@users.noreply.github.com>
2024-04-13 17:16:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+let loadMoreBtn1 = document.querySelector('#load-more-1');
+let currentItem1 = 4;
+
+loadMoreBtn1.onclick = () => {
+let boxes = [...document.querySelectorAll(
+'.box-container-1 .box-1'
+)];
+for(var i = currentItem1; i < currentItem1 + 4; i++) {
+boxes [i]. style.display = 'inline-block';
+}
+currentItem1 += 4;
+if(currentItem1 >= boxes.length){
+    loadMoreBtn1.style.display='none'
+}
+}
+
+let loadMoreBtn2 = document.querySelector('#load-more-2');
+let currentItem2 = 4;
+
+loadMoreBtn2.onclick = () => {
+let boxes = [...document.querySelectorAll(
+'.box-container-2 .box-2'
+)];
+for(var i = currentItem2; i < currentItem2 + 4; i++) {
+boxes [i]. style.display = 'inline-block';
+}
+currentItem2 += 4;
+if(currentItem2 >= boxes.length){
+    loadMoreBtn2.style.display='none'
+}
+}
+
+let loadMoreBtn3 = document.querySelector('#load-more-3');
+let currentItem3 = 4;
+
+loadMoreBtn3.onclick = () => {
+let boxes = [...document.querySelectorAll(
+'.box-container-3 .box-3'
+)];
+for(var i = currentItem3; i < currentItem3 + 4; i++) {
+boxes [i]. style.display = 'inline-block';
+}
+currentItem3 += 4;
+if(currentItem3 >= boxes.length){
+    loadMoreBtn3.style.display='none'
+}                
+}
+
+const prevBtn = document.querySelector('.btn-prev');
+    const nextBtn = document.querySelector('.btn-next');
+    const images = document.querySelectorAll('.imagen-fondo');
+    // Inicializar el índice de la imagen actual
+    let currentImageIndex = 0;
+    // Función para mostrar la imagen siguiente
+    const showNextImage = () => {
+        images[currentImageIndex].style.opacity = '0';
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        images[currentImageIndex].style.opacity = '1';
+    };
+    // Función para mostrar la imagen anterior
+    const showPrevImage = () => {
+        images[currentImageIndex].style.opacity = '0';
+        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+        images[currentImageIndex].style.opacity = '1';
+    };
+    // Agregar event listener para avanzar y retroceder
+    nextBtn.addEventListener('click', showNextImage);
+    prevBtn.addEventListener('click', showPrevImage);
+
+
+
+
